8ea13eb75ee3c4497c96766355627de4293dc277
Alexander Khrisna Satyadhana <42533755+Alexyzyz@users.noreply.github.com>
2023-05-17 10:59:50 +0700

    Chore: Implement barebones hero manager
    I only commited this as an example for kopi

--- RolePlayingCardGame/scripts/battle_manager.gd
+++ RolePlayingCardGame/scripts/battle_manager.gd
@@ -6,8 +6,10 @@ var turn_queue: Array[HeroUnit] = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var hero: HeroUnit = HeroUnit.new();
+	hero.spawn(Vector3(-2, 0, 0));
+	
+	var enemy: HeroUnit;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
